3df736bee0cec37bf0815fa3eeb8e027bef292fb
hussain-sharif <99sharif00@gmail.com>
2025-10-08 23:20:25 +0530

    Made little Progress

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Hussain-Sharif/GO-RSS-Aggregator
 
 go 1.25.1
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"github.com/joho/godotenv"
 )
 
 
 func main(){
 	fmt.Println("Hello World")
+
+	godotenv.Load(".env")
 
 	portString:=os.Getenv("PORT")
 	if(portString==""){
