935ed408bee78d6e02e1c16ab113f6a336326309
hussain-sharif <99sharif00@gmail.com>
2025-10-09 22:31:48 +0530

    Added Some Packages to start Server

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"os"
 	"github.com/joho/godotenv"
+	"github.com/go-chi/chi"
+	// "github.com/go-chi/cors"
 )
 
 
@@ -16,6 +18,18 @@ func main(){
 	portString:=os.Getenv("PORT")
 	if(portString==""){
 		log.Fatal("Port is not found") // This will stop the program to execute
+	}
+
+	router:= chi.NewRouter()
+
+	srv:= &http.Server{
+		Handler:router,
+		Addr:":"+portString,
+	}
+
+	err=srv.ListenAndServe()
+	if(err!=nil){
+		log.Fatal(err)
 	}
 
 	fmt.Println(portString)
