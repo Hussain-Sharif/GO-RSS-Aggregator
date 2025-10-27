5a9a43b910829430f89c20dc976da4ba8d26db96
hussain-sharif <99sharif00@gmail.com>
2025-10-28 00:02:04 +0530

    Created Chi Router

---
+++ json.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	if(code>499){
+		log.Printf("Responding with 5XX err Message: %s",message)
+	}
+
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+	respondWithJSON(w, code, errorResponse{Error: message})
+
+}
+
+func respondWithJSON(w http.ResponseWriter, code int , payload interface{}) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Failed to Marshal the JSON %v",payload)
+		w.WriteHeader(http.StatusInternalServerError) // here we mention the status code
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
--- main.go
+++ main.go
@@ -3,31 +3,49 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
-	"github.com/joho/godotenv"
+	"github.com/go-chi/cors"
 	"github.com/go-chi/chi"
-	// "github.com/go-chi/cors"
+	"github.com/joho/godotenv"
 )
 
 
 func main(){
 	fmt.Println("Hello World")
 
-	godotenv.Load(".env")
+	godotenv.Load(".env") // This will load the .env file
 
-	portString:=os.Getenv("PORT")
-	if(portString==""){
+	portString:=os.Getenv("PORT") // Getting the PORT from the .env file
+	if(portString==""){ // if don't find the PORT
 		log.Fatal("Port is not found") // This will stop the program to execute
 	}
 
-	router:= chi.NewRouter()
+	router:= chi.NewRouter() // Creating a new router
 
-	srv:= &http.Server{
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*","http://*"},
+		AllowedMethods: []string{"GET","POST","PUT","DELETE","PATCH"},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: false,
+		MaxAge: 300,
+	}))
+
+	v1Router:=chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.HandleFunc("/err",handlerError)
+
+
+	router.Mount("/v1",v1Router)
+
+	srv:= &http.Server{    // Creating a new server with that created router
 		Handler:router,
 		Addr:":"+portString,
 	}
 
-	err=srv.ListenAndServe()
+	log.Printf("Server Starting at port %v", portString) 
+	err:=srv.ListenAndServe()
 	if(err!=nil){
 		log.Fatal(err)
 	}
