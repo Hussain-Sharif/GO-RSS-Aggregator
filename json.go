034bb8d25afba287cc3e6f7f60d4a1a15043e4b0
Hussain Sharif <99sharif00@gmail.com>
2025-11-06 16:57:22 +0000

    Made syntactical Insights

--- json.go
+++ json.go
@@ -12,13 +12,13 @@ func respondWithError(w http.ResponseWriter, code int, message string) {
 	}
 
 	type errorResponse struct {
-		Error string `json:"error"`
+		Error string `json:"error"` // json:"desiredJSONKeyName" --> t's metadata that tells the JSON encoder/decoder how to handle this field when converting between Go structs and JSON.
 	}
-	respondWithJSON(w, code, errorResponse{Error: message})
+	respondWithJSON(w, code, errorResponse{Error: message}) // here the errorResponse{} is exactly the instance of that type with proper values
 }
 
 // Below one is like a template for the boilerplate for proper json response
-func respondWithJSON(w http.ResponseWriter, code int , payload interface{}) {
+func respondWithJSON(w http.ResponseWriter, code int , payload interface{}) { // here interface payload accepts any type as the payload argument 
 	data, err := json.Marshal(payload)
 	
 	if err != nil {
